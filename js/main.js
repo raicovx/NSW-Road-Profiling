2b33dce12906de2e11a1f4891c43da0a53be8db4
Jamie <james.cranston91@gmail.com>
2015-05-13 14:02:44 +1000

    pt 2 of previous commit

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
              sliderCont.animate({height:"400px"}, 1000);
             });
             sliderCont.toggleClass('fullscreen');
-            navBar.show().animate({top:"-100px"},1000);
+            navBar.show().animate({top:"0px"},1000);
         isFullScreen = false;
             $('html, body').css({
     'overflow': 'auto',
@@ -28,13 +28,13 @@ $(document).ready(function(){
             indic.css('top','');
           sliderCont.toggleClass('fullscreen');
         isFullScreen = true;
-        navBar.animate({top:"0"},1000).hide();
+        navBar.animate({top:"-100px"},1000).delay(1000).hide(0);
             $('html, body').css({
     'overflow': 'hidden',
     'height': '100%'
                 });
             fullScreenButtn.css('top', '75%');
-            $('.welcomeArea').animate({marginTop: '50%'}, 1050);
+            $('#one').animate({marginTop: '50%'}, 1050);
             indic.animate({top:'750px'},1000);
         }
 
