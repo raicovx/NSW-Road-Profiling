acc35320933b4e9e48cee13df71fbfa076d113cd
Jamie <james.cranston91@gmail.com>
2015-05-14 23:22:11 +1000

    Styling changes, parallax content added

--- parallaxOne.php
+++ parallaxOne.php
@@ -2,12 +2,12 @@
   <div class="section no-pad-bot">
     <div class="container">
       <br><br>
-      <h1 class="header center blue-text">Parallax Template</h1>
-      <div class="row center">
-        <h5 class="header col s12 white-text light">A modern responsive front-end framework based on Material Design</h5>
+      <h1 class="header left-align blue-text">Get in touch!</h1>
+      <div class="row left-alignr">
+        <h5 class="header col s12 white-text light">You can use the buttons(or information) below to contact us!</h5>
       </div>
-      <div class="row center">
-        <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light blue">Get Started</a>
+      <div class="row left-align">
+        <a href="#" id="email-modal" class="btn-large parallButton waves-effect waves-light blue">Email us!</a><a href="#" id="email-modal" class="btn-large parallButton waves-effect waves-light blue">Call Us - 0400 000 000</a>
       </div>
       <br><br>
 
